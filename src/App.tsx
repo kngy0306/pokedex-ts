bdce2d1ff53d49e6ff92cba6f8ce3aec961e2e00
Kona <kitami.kitano20@gmail.com>
2021-12-12 14:45:55 +0900

    :construction: ERR Objects are not valid as a react child

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChakraProvider, Container } from '@chakra-ui/react';
 import { theme } from './theme/Theme';
 
 import Header from './components/Header';
-import Main from './components/Main';
+// import Main from './components/Main';
 import Footer from './components/Footer';
 
+type PokemonData = {
+  id: number;
+  name: string;
+  weight: number;
+  type: string;
+  image: string;
+  animated_image: string;
+};
+
+type Types = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
 const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<PokemonData[]>([]);
+
+  const getPokemon = async () => {
+    try {
+      for (let id = 387; id <= 389; id++) {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        if (!response.ok) {
+          throw new Error('Found no Pokemon!');
+        }
+
+        const data = await response.json();
+        const type: string = data.types
+          .map((d: Types) => d.type.name)
+          .join(', ');
+
+        const pokemon: PokemonData = {
+          id: data.id,
+          name: data.name,
+          weight: data.weight,
+          type: type,
+          image: data.sprites.other['official-artwork'].front_default,
+          animated_image:
+            data.sprites.versions['generation-v']['black-white'].animated
+              .front_default,
+        };
+        console.log(pokemon);
+
+        setPokemons((prevPokemons) => {
+          return [...prevPokemons, pokemon];
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, [pokemons]);
+
   return (
     <ChakraProvider theme={theme}>
       <Container maxW="container.md">
         <Header />
-        <Main />
+        {pokemons}
         <Footer />
       </Container>
     </ChakraProvider>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+type PokemonData = {
+  id: number;
+  name: string;
+  weight: string;
+  type: string;
+  image: string;
+  animated_image: string;
+};
+
+type Types = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
 
 const Main: React.FC = () => {
+  console.log('main');
+  const [pokemons, setPokemons] = useState<PokemonData[]>([]);
+
+  const FetchPokemon = async () => {
+    try {
+      for (let id = 387; id <= 389; id++) {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        if (!response.ok) {
+          throw new Error('Found no Pokemon!');
+        }
+
+        const data = await response.json();
+        const type = data.types.map((d: Types) => d.type.name).join(', ');
+
+        const pokemon: PokemonData = {
+          id: data.id,
+          name: data.name,
+          weight: data.weight,
+          type: type,
+          image: data.sprites.other['official-artwork'].front_default,
+          animated_image:
+            data.sprites.versions['generation-v']['black-white'].animated
+              .front_default,
+        };
+
+        setPokemons((prevPokemons) => {
+          return [...prevPokemons, pokemon];
+        });
+      }
+
+      console.log(pokemons);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    FetchPokemon();
+  });
+
   return <h1>React</h1>;
 };
 
